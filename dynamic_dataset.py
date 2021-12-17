e4f9f07e2f4afc60b97e90057a37f43adfda4658
Guy <guyk1994@gmail.com>
2021-12-17 21:14:19 +0200

    fixed frame index mixed with sample index

--- dynamic_dataset.py
+++ dynamic_dataset.py
@@ -35,25 +35,19 @@ class DynamicFrameDataset(Dataset):
         path = self.x[idx]
         frames = []
 
-        print(path)
         cap = cv2.VideoCapture(path)   #   create video object
         v_len = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))              #   get number of frames
-        frame_idx = []
-        count = 0
+        frame_idx = np.random.choice(np.arange(v_len-1), self.num_frames)   #   get random frame indices, sometimes the last frame generates an error, therefore v_len-1
 
-        while count < self.num_frames:
-            idx = np.random.randint(0, v_len)
-            if idx in frame_idx:
-                continue
-            frame_idx.append(idx)
-            cap.set(cv2.CAP_PROP_POS_FRAMES, idx)
+
+        for i in frame_idx:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, i)
             ret, frame = cap.read()
             if ret:
                 img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB).astype(np.float32)
                 if self.transforms:   
                     img = self.transforms(image=img)['image']
                 frames.append(img)
-                count += 1
 
         cap.release()
         
@@ -69,13 +63,23 @@ if __name__ == "__main__":
     train_path = 'data/train/'
     valid_path = 'data/valid/'
 
-    train_dataset = DynamicFrameDataset(train_path, 16, get_transformer('valid'))
+    train_dataset = DynamicFrameDataset(valid_path, 16, get_transformer('valid'))
     print(len(train_dataset))
     print(len(train_dataset.x), len(train_dataset.y))
 
     batch = train_dataset.__getitem__(30)
     x, y = batch
-    print(x[0])
+    # print(x[0])
     print(x.size(), y)
+    print(len(train_dataset.x), len(train_dataset.y))
+
+    loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=8,
+        shuffle= True,
+        num_workers=8
+    )
+
+    batch = next(iter(loader))
     # imgs, lbls = batch
     # print(imgs.shape)
